26a3fbdf7df3fc4d6c528ac03d058310fa651dea
Stephen Peck <speck1990@gmail.com>
2020-06-04 21:24:37 -0500

    Setup redux and add actions

---
+++ src/reducers/logReducer.js
@@ -0,0 +1,42 @@
+import { GET_LOGS, ADD_LOG, SET_LOADING, LOGS_ERROR } from "../actions/types";
+
+const initialState = {
+	logs: null,
+	current: null,
+	loading: false,
+	error: null
+};
+
+export default (state = initialState, action) => {
+	switch (action.type) {
+		case GET_LOGS:
+			return {
+				...state,
+				logs: action.payload,
+				loading: false
+			};
+
+		case ADD_LOG:
+			return {
+				...state,
+				logs: [...state.logs, action.payload],
+				loading: false
+			};
+
+		case SET_LOADING:
+			return {
+				...state,
+				loading: true
+			};
+
+		case LOGS_ERROR:
+			console.error(action.payload);
+			return {
+				...state,
+				error: action.payload
+			};
+
+		default:
+			return state;
+	}
+};
